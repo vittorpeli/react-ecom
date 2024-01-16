b4faf599c7ac18daece6156bfa94fe31ee10163f
vittor <vittor.pelicarto137@gmail.com>
2024-01-16 16:55:12 -0300

    update card style

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -4,11 +4,12 @@ import { Button } from "./Button";
 
 export const Card = (props) => {
   return (
-    <Box className="cursor-pointer py-2 px-3 hover:shadow-xl focus:shadow-xl">
-      <Stack className="flex-grow last:mt-auto [&>*:nth-last-child(2)]:mb-3">
-        <div className="h-20">
+    <Box className="cursor-pointer hover:shadow-xl focus:shadow-xl">
+      <div className="h-20">
           <img className="object-cover h-full w-full" src={props.url} alt={props.name} />
-        </div>
+      </div>
+      <Stack className="py-2 px-3 flex-grow last:mt-auto [&>*:nth-last-child(2)]:mb-3">
+        
         
         <div className="flex flex-col flex-grow last:mt-auto [&>*:nth-last-child(2)]:mb-3">
           <h2>
