8ba5990959c511d205a25898f01f01688497a43e
vittor <vittor.pelicarto137@gmail.com>
2024-01-16 14:39:38 -0300

    change button style

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,20 +1,29 @@
 
-export const Button = ({ variant, exception, children, onClick }) => {
+export const Button = ({ variant, children, onClick }) => {
   const getStyles = () => {
     // Default styles
     let styles = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      whiteSpace: 'nowrap',
+      height: '32px',
+      padding: '8px 12px',
+      fontSize: '14px',
+      lineHeight: '1.2',
+      fontWeight: '500',
       backgroundColor: 'blue',
       color: 'white',
       border: 'none',
-      padding: '8px 12px',
       cursor: 'pointer',
       textDecoration: 'none',
+      transition: 'background 20ms ease-in 0s'
     };
 
     // Variant-specific styles
     if (variant === 'ghost') {
       styles.backgroundColor = 'transparent';
-      styles.border = '1px solid white';
+      styles.border = '1px solid rgba(255, 255, 255, 0.13)';
     } else if (variant === 'link') {
       styles.backgroundColor = 'transparent';
       styles.textDecoration = 'underline';
