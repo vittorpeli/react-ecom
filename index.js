1e0b059346bfa152258142e5228799bfc3f7b80d
vittor <vittor.pelicarto137@gmail.com>
2024-01-19 18:49:02 -0300

    use cors());

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // import path from 'path'
 import express  from 'express'
 import router from './server/router.js';
+import cors from 'cors';
 
 const app = express();
 
 const port = 3001;
+
+app.use(cors());
 
 app.use(express.json());
 
