6a176053fa73cd5a9834be487ead9f7111b26901
vittor <vittor.pelicarto137@gmail.com>
2024-01-21 01:15:58 -0300

    update Sidebar

--- src/components/layouts/Sidebar/Sidebar.jsx
+++ src/components/layouts/Sidebar/Sidebar.jsx
@@ -1,17 +1,23 @@
 import './Sidebar.css';
 
-export const Sidebar = ({ side = 'left', sideWidth, contentMin = '50%', space = 'var(--s1)', noStretch, children, className }) => {
-  const sidebarClass = `sidebar ${side === 'right' ? 'right' : ''} ${className || ''}`;
-  const style = {
-    '--side': side,
+export const Sidebar = ({ 
+  side = 'left', 
+  sideWidth, 
+  contentMin = '50%', 
+  space = 'var(--s1)', 
+  noStretch, 
+  children, 
+  className 
+}) => {
+  const sidebarClass = `sidebar ${className || ''}`;
+  const styles = {
     '--sideWidth': sideWidth || 'auto',
     '--contentMin': contentMin,
-    '--space': space,
-    '--alignItems': noStretch ? 'flex-start' : 'stretch',
+    '--space': space === '0' ? '0px' : space,
   };
 
   return (
-    <div className={sidebarClass} style={style}>
+    <div className={`${sidebarClass} ${side === 'right' ? 'right' : ''} ${noStretch ? noStretch : ''}`} style={styles}>
       {side === 'right' ? children.reverse() : children}
     </div>
   );
--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,3 +1,20 @@
+import { Stack } from "../components/layouts/Stack/Stack"
+import { Wrapper } from "../components/layouts/Wrapper/Wrapper"
+import { Navbar } from "../components/ui/Navbar"
+
 export const SuccessPage = () => {
-  
+  return (
+    <Wrapper>
+      <Stack>
+        <div>
+          <Navbar />
+        </div>
+
+        <Wrapper>
+          <h1>Checkout Complete</h1>
+          <p>Thanks for your purchase!</p>
+        </Wrapper>
+      </Stack>
+    </Wrapper>
+  )
 }
