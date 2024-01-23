98cac46bb5b9521d3c05cc16c11514b948ce1d73
vittor <vittor.pelicarto137@gmail.com>
2024-01-23 03:15:21 -0300

    Update API

--- server/controllers/PhotosController.js
+++ server/controllers/PhotosController.js
@@ -1,22 +1,89 @@
-// import { photos } from "../utils/photos.js";
+import { getPhotos, getSelectedPhoto, destroy, findByName, create, postUpdate } from "../repositories/PhotosRepository.js";
 
-function index (req, res) {
-  res.send({ message: 'Hello World'})
+async function index (req, res) {
+  const photos = await getPhotos();
+
+  res.json(photos);
 }
 
-function show () {
+async function show (req, res) {
+  const { id } = req.params;
 
+  const photo = await getSelectedPhoto(id);
+
+  if(!photo) {
+    return res.status(404).send("Photo not found!");
+  }
+
+  res.json(photo);
 }
 
-function store () {
+async function store (req, res) {
+  const { ID, name, url, description, price } = req.body;
 
+  if (!name) {
+    return res.status(400).json("Photo's name is required!");
+  }
+
+  const photoExists = await findByName(name);
+
+  if(photoExists) {
+    return res.status(409).json("Photo's name is already in use!");
+  }
+
+  if(!url) {
+    return res.status(400).json("Photo's url is required!");
+  }
+
+  if (!price) {
+    return res.status(400).json("Photo's price is required!");
+  }
+
+  const photo = await create({
+    ID, name, url, description, price
+  })
+
+  res.json(photo);
 }
 
-function update() {
+async function update(req, res) {
+  const { id } = req.params;
+  const { ID, name, url, description, price } = req.body;
 
+  const photoExists = await getSelectedPhoto(id);
+  if(!photoExists) {
+    return res.status(404).json("Photo not found!");
+  }
+
+  if (!name) {
+    return res.status(400).json("Photo's name is required!");
+  }
+
+  const photoByName = await findByName(name);
+
+  if(photoByName && photoByName.ID !== id) {
+    return res.status(409).json("Photo's name is already in use!");
+  }
+
+  const photo = await postUpdate({
+    ID, name, url, description, price
+  })
+
+  res.json(photo);
 }
 
-function del () {
+async function del (req, res) {
+  const { id } = req.params;
+
+  const photo = await getSelectedPhoto(id);
+
+  if (!photo) {
+    return res.status(404).send("Photo not found!");
+  }
+
+  await destroy(id);
+
+  res.sendStatus(204);
 
 }
 
---
+++ server/repositories/PhotosRepository.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-useless-catch */
+
+// let photos = [
+//   {
+//     id: 'boat-at-midnight',
+//     name: 'Boat at Midnight',
+//     url: 'https://unsplash.com',
+//     description: 'A boat at midnight',
+//     price: 23,
+//   }
+// ];
+
+export async function getPhotos() {
+
+  const API = "https://vanillajsacademy.com/api/photos.json";
+
+  try {
+    const response = await fetch(API);
+    const photosData = await response.json();
+    return photosData;
+  } catch (error){
+    throw error;
+  } 
+}
+
+export async function getSelectedPhoto(id) {
+  try {
+    const photosData = await getPhotos();
+    const selectedPhoto = photosData.find((p) => p.id === id);
+
+    if(selectedPhoto) {
+      return selectedPhoto;
+    }else {
+      console.error("Photo not found");
+      return null;
+    } 
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function destroy(id) {
+  const photos = await getPhotos();
+  const filteredPhoto = photos.filter((p) => p.id !== id);
+
+  return filteredPhoto;
+}
+
+export async function findByName(name) {
+  const photos = await getPhotos();
+  const selectedPhoto = photos.find((p) => p.name === name);
+
+  return selectedPhoto;
+}
+
+export async function create({ ID, name, url, description, price }) {
+  const photos = await getPhotos();
+
+  const newPhoto = {
+    ID, name, url, description, price
+  }
+
+  photos.push(newPhoto);
+
+  return newPhoto;
+}
+
+export async function postUpdate({ ID, name, url, description, price}) {
+  const photos = await getPhotos();
+
+  const updatedPhoto = {
+    ID, name, url, description, price
+  };
+
+  photos.map((p) => p.id === ID ? updatedPhoto : p);
+
+  return updatedPhoto;
+}
